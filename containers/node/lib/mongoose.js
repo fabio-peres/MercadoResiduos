4d3a6a8bbb02c6d79f2069a06d1347c8ffd3f7a1
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-03 00:14:02 -0300

    atualizacao credenciais mongo

--- containers/node/lib/mongoose.js
+++ containers/node/lib/mongoose.js
@@ -28,10 +28,10 @@ module.exports = function () {
      */
     const getConfig = () => {
         const config = {
-            database: process.env.NODE_MONGO_DBNAME || "monitor",
-            host: process.env.NODE_MONGO_HOST || 'ec2-34-194-146-84.compute-1.amazonaws.com',
-            user: process.env.NODE_MONGO_USER || '',
-            pass: process.env.NODE_MONGO_PASS || '',
+            database: process.env.NODE_MONGO_DBNAME || "mercado",
+            host: process.env.NODE_MONGO_HOST || 'mercado',
+            user: process.env.NODE_MONGO_USER || 'mercado',
+            pass: process.env.NODE_MONGO_PASS || 'mercado123!',
             port: process.env.NODE_MONGO_PORT || 27017
         };
         return config;
