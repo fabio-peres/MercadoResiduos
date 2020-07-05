bbfa0f8a6069d7b2d22982d2e1b61d242c439d0e
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-05 18:15:02 -0300

    migration criacao tabela endereco

---
+++ containers/node/migrations/20200705210649-endereco.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+    CREATE TABLE public.endereco (
+      id SERIAL PRIMARY KEY,
+      rua VARCHAR(100) NOT NULL,
+      numero VARCHAR(10) NOT NULL,
+      bairro VARCHAR(100) NOT NULL,
+      cep VARCHAR(15) NOT NULL);`
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`DROP TABLE public.endereco`);
+  }
+};
