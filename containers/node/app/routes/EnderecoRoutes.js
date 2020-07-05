d741d06077b8ab718f4a2cc17584a033ce18db40
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-05 19:54:11 -0300

    atualizacao rota obter agencia correios mais proxima

--- containers/node/app/routes/EnderecoRoutes.js
+++ containers/node/app/routes/EnderecoRoutes.js
@@ -2,6 +2,6 @@ module.exports = function (app) {
     const UserController = app.controllers.EnderecoController;
     app.route('/api/endereco')
         .post(UserController.cadastrarEnderecoUsuario);
-    app.route('/api/correios/:id')
+    app.route('/api/endereco/correios/:id')
         .get(UserController.buscarAgenciaCorreios);
 }
