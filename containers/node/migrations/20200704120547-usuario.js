7f634974f369a481718c708b42ac3fbeb2b1f1d8
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-05 14:18:17 -0300

    ajuste migration criacao usuario

--- containers/node/migrations/20200704120547-usuario.js
+++ containers/node/migrations/20200704120547-usuario.js
@@ -7,7 +7,7 @@ module.exports = {
       id SERIAL PRIMARY KEY,
       nome VARCHAR(50) NOT NULL,
       email VARCHAR(50) NOT NULL,
-      documento VARCHAR(50) NOT NULL`
+      documento VARCHAR(50) NOT NULL);`
     );
   },
 
