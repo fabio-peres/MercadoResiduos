313d3a550b3adef4878dfbc99e7a1d81b786d60b
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-05 17:07:14 -0300

    atualizacao migration

--- containers/node/migrations/20200704120547-usuario.js
+++ containers/node/migrations/20200704120547-usuario.js
@@ -8,6 +8,7 @@ module.exports = {
       nome VARCHAR(50) NOT NULL,
       email VARCHAR(50) NOT NULL,
       senha VARCHAR(100) NOT NULL,
+      categoria CHAR(1) NOT NULL,
       documento VARCHAR(50) NOT NULL);`
     );
   },
