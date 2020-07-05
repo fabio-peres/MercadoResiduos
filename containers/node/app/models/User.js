33e0c3b27bfb54dda2b34e05d3ccad6d85fe8689
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-05 12:56:30 -0300

    usuario model

--- containers/node/app/models/User.js
+++ containers/node/app/models/User.js
@@ -1,6 +1,31 @@
-const Mongoose = require('mongoose');
-module.exports = function(app) {
-    return app.mongoose.model('User', new Mongoose.Schema({
-
-    },{ collection: 'User' }));
-}
+const Sequelize = require('sequelize');
+module.exports = function (app) {
+    const User = app.connector.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        documento: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
+    }, {
+        //Desable fields for not create automatic into table
+        createdAt: true,
+        updatedAt: true,
+        deletedAt: false,
+        freezeTableName: true,
+        tableName: 'user'
+    });
+    //Return
+    return User;
+};
