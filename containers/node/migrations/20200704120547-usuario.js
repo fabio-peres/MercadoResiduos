d467d9547a52534c59bcff16092e2aab3f41b6b5
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-05 13:25:23 -0300

    criacao de migration de usuario

---
+++ containers/node/migrations/20200704120547-usuario.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+    CREATE TABLE public.user (
+      id SERIAL PRIMARY KEY,
+      nome VARCHAR(50) NOT NULL,
+      email VARCHAR(50) NOT NULL,
+      documento VARCHAR(50) NOT NULL`
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`DROP TABLE public.user`);
+  }
+};
