ffcdbd2538fb1b6e65debf55ef7ff4f4efa996a7
João Paulo <lcjoaopaulo@pop-os.localdomain>
2020-07-05 14:18:08 -0300

    criacao de midleware para validacao de usuario autenticado

--- containers/node/app/controllers/UserController.js
+++ containers/node/app/controllers/UserController.js
@@ -39,5 +39,49 @@ module.exports = function (app) {
         }
     }
 
+    /**
+     * authorizeRequest
+     * @param {Object} req
+     * @param {Object} res
+    */
+    _self.authorizeRequest = async (req, res, next) => {
+        const { authorization } = req.headers;
+
+        if (!authorization) {
+            return res.status(401).json({
+                errors: ['Login required'],
+            });
+        }
+
+        const [, token] = authorization.split(' ');
+
+        try {
+            const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+            const { id, email } = dados;
+
+            const user = await User.findOne({
+                where: {
+                    id,
+                    email,
+                },
+            });
+
+            if (!user) {
+                return res.status(401).json({
+                    errors: ['Usuário inválido'],
+                });
+            }
+
+            req.userId = id;
+            req.userEmail = email;
+            return next();
+        } catch (e) {
+            return res.status(401).json({
+                errors: ['Token expirado ou inválido.'],
+            });
+        }
+
+    }
+
     return _self;
 }
